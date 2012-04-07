72e0c0b4d4680b7a7b4b42be525d406635dae40f
Henry Zhao <henry.zhao@oracle.com>
2012-04-06 17:26:59 -0700

    scanpci: print meaningful info on BASEROM
    Signed-off-by: Henry Zhao <henry.zhao@oracle.com>
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- scanpci/scanpci.c
+++ scanpci/scanpci.c
@@ -47,6 +47,7 @@
 #endif
 
 #include "pciaccess.h"
+#include "pciaccess_private.h"
 
 
 static void
@@ -168,8 +169,11 @@ print_pci_device( struct pci_device * dev, int verbose )
 	}
 
 	if ( dev->rom_size ) {
-	    printf( "  BASEROM   0x%08x  addr 0x%08x\n",
-		    0, 0 );
+	    struct pci_device_private *priv =
+		(struct pci_device_private *) dev;
+
+		printf( "  BASEROM   0x%08"PRIxPTR" SIZE %zu\n",
+			(intptr_t) priv->rom_base, (size_t) dev->rom_size);
 	}
 
 	pci_device_cfg_read_u8( dev, & int_pin, 61 );
