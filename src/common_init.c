6bfccc7ec4f0705595385f6684b6849663f781b4
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2014-01-18 17:19:14 +0100

    Windows/Cygwin: Add support through the WinIo library
    V2:
    - Add support for unmapping
    - Add a README.cygwin
    
    Reviewed-by: Martin Peres <martin.peres@free.fr>
    Reviewed-by: Jon TURNEY <jon.turney@dronecode.org.uk>

--- src/common_init.c
+++ src/common_init.c
@@ -62,7 +62,7 @@ pci_system_init( void )
     err = pci_system_openbsd_create();
 #elif defined(__sun)
     err = pci_system_solx_devfs_create();
-#elif defined(__GNU__)
+#elif defined(__GNU__) || defined(__CYGWIN__)
     err = pci_system_x86_create();
 #endif
 
