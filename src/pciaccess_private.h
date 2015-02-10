e42ee2a075a8b7c62b190815be42ea26c9d8c03f
Emil Velikov <emil.l.velikov@gmail.com>
2015-02-10 14:55:19 +0000

    Include the POSIX fcntl.h instead of sys/fcntl.h
    The former is part of the POSIX standard, and (unlike the latter) is
    more widely available.
    
    v2: Drop gracious d in header name. Spotted by Alan.
    
    Cc: Adam Jackson <ajax@redhat.com>
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- src/pciaccess_private.h
+++ src/pciaccess_private.h
@@ -42,7 +42,7 @@
  * know of any OS we support where this isn't available in a sufficiently
  * new version, so warn unconditionally.
  */
-#include <sys/fcntl.h>
+#include <fcntl.h>
 
 #ifndef O_CLOEXEC
 #warning O_CLOEXEC not available, please upgrade.
